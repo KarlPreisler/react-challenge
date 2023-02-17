1d5df3bbe6634e0481ea5fd1d7176158870bd2f3
KarlPreisler <Karlpreisler@gmail.com>
2023-02-17 12:48:05 +0000

    Add ClassAPI and convert into functional component using hooks

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,16 +2,21 @@ import css from "./App.module.css";
 // import NavBarSimple from "./components/NavBarSimple";
 import Sidebar from "./components/Sidebar";
 import NavBarForm from "./components/NavBarForm";
-import Content from "./components/Content";
-import ContentHooks from "./components/ContentHooks";
+//import Content from "./components/Content";
+// import ContentHooks from "./components/ContentHooks";
+// import ContentAPI from "./components/ContentAPI";
+import ContentAPIHooks from "./components/ContentAPIHooks";
 
 function App() {
   return (
     <div className={css.App}>
+      {/* Add your components here */}
       <NavBarForm />
       <Sidebar />
-      {/*<Content /> */}
-      <ContentHooks />
+      {/* <Content /> */}
+      {/* <ContentHooks /> */}
+      {/* <ContentAPI /> */}
+      <ContentAPIHooks />
     </div>
   );
 }
